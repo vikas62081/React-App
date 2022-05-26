d323af839ac28bf9585265c34944a3351dc7ad56
Vikas Kumar Vishwakarma <vikas@vikass-MacBook-Air.local>
2022-05-26 21:17:01 +0530

    pushing different environment in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log(process.env.REACT_APP_API_URL);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Handle Different Enviromnent in a React App</h2>
+      <h3>{process.env.REACT_APP_NAME}</h3>
+      <h3>Env : {process.env.REACT_APP_ENV}</h3>
     </div>
   );
 }
